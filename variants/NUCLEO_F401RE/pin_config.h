dde14c87166f8a4c9f8c5e6e5cb9b2373c1435e1
NaotoFujihiro <naoto.fujihiro@gmail.com>
2020-01-25 01:55:39 +0900

    Add pin_config files for motor driver ic's

---
+++ variants/NUCLEO_F401RE/pin_config.h
@@ -0,0 +1,43 @@
+#define PIN_A1_DIR          PC6
+#define PIN_A1_STEP         PC5
+#define PIN_A2_DIR          PB15
+#define PIN_A2_STEP         PB14
+#define PIN_B_DIR           PB2
+#define PIN_B_STEP          PB1
+#define PIN_C_DIR           PA12
+#define PIN_C_STEP          PA11
+#define PIN_D_DIR           PB12
+#define PIN_D_STEP          PB13
+
+#define PIN_0_DIR           PC2
+#define PIN_0_STEP          PC3
+#define PIN_1_DIR           PH0
+#define PIN_1_STEP          PH1
+#define PIN_2_DIR           PC13
+#define PIN_2_STEP          PC14
+#define PIN_3_DIR           PA13
+#define PIN_3_STEP          PA14
+#define PIN_4_DIR           PB4
+#define PIN_4_STEP          PB5
+#define PIN_5_DIR           PB8
+#define PIN_5_STEP          PB9
+#define PIN_6_DIR           PC1
+#define PIN_6_STEP          PC0
+#define PIN_7_DIR           PA0
+#define PIN_7_STEP          PA1
+
+
+
+// PA0, PA1, PA4, PA13, PA14, PA15
+// PA2, PA3, PA4, PA6, PA7, PA10, PA11, PA12
+
+// PB1, PB2, PB3, PB4, PB5, PB6, PB8, PB9, PB10, PB12, PB14, PB15
+// PB7, PB8, PB9
+
+// PC0, PC1, PC2, PC3, PC10, PC11, PC12, PC13, PC14, PC15
+// PC4, PC5, PC6, PC7, PC8, PC9
+
+// PD2
+
+// PH0, PH1
+
