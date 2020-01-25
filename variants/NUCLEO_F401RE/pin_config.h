73035ac898606b4488a58a65f2075c8386d6ca70
NaotoFujihiro <naoto.fujihiro@gmail.com>
2020-01-25 19:50:19 +0900

    Add limit switch pin to pin_config.h:

--- variants/NUCLEO_F401RE/pin_config.h
+++ variants/NUCLEO_F401RE/pin_config.h
@@ -1,40 +1,42 @@
 #define PIN_A1_DIR          PC6
 #define PIN_A1_STEP         PC5
+#define PIN_A1_LIMIT        PC8
 #define PIN_A2_DIR          PB15
 #define PIN_A2_STEP         PB14
+#define PIN_A2_LIMIT        PC4
 #define PIN_B_DIR           PB2
 #define PIN_B_STEP          PB1
+#define PIN_B_LIMIT         PA11
 #define PIN_C_DIR           PA5
 #define PIN_C_STEP          PA6
+#define PIN_C_LIMIT         PA7
 #define PIN_D_DIR           PB12
 #define PIN_D_STEP          PB13
+#define PIN_D_LIMIT         PA12
 
 #define PIN_0_DIR           PC2
 #define PIN_0_STEP          PC3
+#define PIN_0_LIMIT         PA10
 #define PIN_1_DIR           PC10
 #define PIN_1_STEP          PC12
+#define PIN_1_LIMIT         PA15
 #define PIN_2_DIR           PA4
 #define PIN_2_STEP          PH1
+#define PIN_2_LIMIT         PB7
 #define PIN_3_DIR           PA13
 #define PIN_3_STEP          PA14
+#define PIN_3_LIMIT         PD2
 #define PIN_4_DIR           PB4
 #define PIN_4_STEP          PB5
+#define PIN_4_LIMIT         PB10
 #define PIN_5_DIR           PB8
 #define PIN_5_STEP          PB9
+#define PIN_5_LIMIT         PC9
 #define PIN_6_DIR           PC1
 #define PIN_6_STEP          PC0
+#define PIN_6_LIMIT         PB3
 #define PIN_7_DIR           PA0
 #define PIN_7_STEP          PA1
+#define PIN_7_LIMIT         PC7
 
 #define MAX_RPM             720
-
-// PA2, PA3, PA7, PA10, PA11, PA12, PA15
-
-// PB3, PB6, PB7, PB8, PB9, PB10
-
-// PC4, PC7, PC8, PC9, PC11, PC13, PC14, PC15
-
-// PD2
-
-// PH0
-
